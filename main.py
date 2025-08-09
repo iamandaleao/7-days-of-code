0e6f6af62dedced2f904bb4114c6d414abc175a6
iamandaleao <amandabibiano144@gmail.com>
2025-08-09 11:26:14 -0300

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://last-airbender-api.fly.dev/api/v1/characters"
+
+# Faz a requisição GET
+response = requests.get(url)
+
+#Verifica se deu tudo certo (status 200= ok)
+if response.status_code == 200:
+  dados = response.json() # Converte para dicionário/lista Python
+  print(dados) # Imprime todo o conteúdo
+else:
+  print(f"Erro na requisição: {response.status_code}")
