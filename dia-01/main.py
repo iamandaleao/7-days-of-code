5628df78a9c5a8a3157e0f3359ed128348efe2d5
iamandaleao <amandabibiano144@gmail.com>
2025-08-12 20:43:15 -0300

    chore: improve API response formatting

--- dia-01/main.py
+++ dia-01/main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 url = "https://last-airbender-api.fly.dev/api/v1/characters"
@@ -8,6 +9,7 @@ response = requests.get(url)
 #Verifica se deu tudo certo (status 200= ok)
 if response.status_code == 200:
   dados = response.json() # Converte para dicionário/lista Python
-  print(dados) # Imprime todo o conteúdo
+  print(json.dumps(dados, indent=4))
 else:
   print(f"Erro na requisição: {response.status_code}")
+
