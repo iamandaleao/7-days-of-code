7f1054252dd8ee2f2a7149bcca15a5f9409aebd9
iamandaleao <amandabibiano144@gmail.com>
2025-08-12 20:47:02 -0300

    feat: add challenge 4

---
+++ dia-04/desafioapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import requests
+
+# Create your views here.
+def index(request):
+    url = "https://last-airbender-api.fly.dev/api/v1/characters"
+
+    # Faz a requisição GET
+    response = requests.get(url)
+
+    # Verifica se deu tudo certo (status 200 = ok)
+    if response.status_code == 200:
+        dados = response.json()  # Converte para dicionário/lista Python
+        return HttpResponse(json.dumps(dados, indent=4), content_type="application/json")
+    else:
+        print(f"Erro na requisição: {response.status_code}")
+        return HttpResponse(f"Erro na requisição: {response.status_code}")
